688c44b6570a5e5ca3ba2c84e9aa33cb58d35bd2
ashumig <ashaniisu99@gmail.com>
2021-09-26 15:08:07 +0700

    feedback

--- app/src/main/java/com/example/moremeal/Feedbackdb.java
+++ app/src/main/java/com/example/moremeal/Feedbackdb.java
@@ -1,18 +1,2 @@
 package com.example.moremeal;
 
-public class Feedbackdb {
-
-    String feedback;
-
-    public Feedbackdb() {
-
-    }
-
-    public String getFeedback() {
-        return feedback;
-    }
-
-    public void setFeedback(String feedback) {
-        this.feedback = feedback;
-    }
-}
