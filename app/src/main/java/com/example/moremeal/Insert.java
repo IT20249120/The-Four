9e6b0b09b4c48ed60a3c858939c201371270d741
ashumig <ashaniisu99@gmail.com>
2021-10-02 09:18:49 +0700

    insert updated

--- app/src/main/java/com/example/moremeal/Insert.java
+++ app/src/main/java/com/example/moremeal/Insert.java
@@ -14,5 +14,37 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class Insert extends AppCompatActivity {
 
+    EditText feedbck;
+    Button button;
+
+    DatabaseReference referenc;
+    Feedbackdb Re;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+
+        button = findViewById(R.id.sbFeedback);
+        feedbck = findViewById(R.id.feedbck);
+
+        Re = new Feedbackdb();
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                referenc = FirebaseDatabase.getInstance().getReference().child("Feedback");
+
+                Re.setFeedback(feedbck.getText().toString().trim());
+
+                referenc.child("feed1").setValue(Re);
+                Toast.makeText(getApplicationContext(), "Added Successfull!..", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
 
 }
